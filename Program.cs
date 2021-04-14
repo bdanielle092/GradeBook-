b1000e47fb8f7fc5fad2e22189bbc770547334b0
Faith Magras <bdanielle092@gmail.com>
2021-04-14 11:10:16 -0500

    made an array and list of grades

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -7,6 +8,27 @@ namespace GradeBook
         //method
         static void Main(string[] args)
         {
+
+            //creating a new array
+            var numbers = new[] { 12.7, 18.7, 19.7, 20.4 };
+            //list of grades
+            var grades = new List<double>() { 12.7, 18.7, 19.7, 20.4 };
+            grades.Add(45.5);
+
+
+
+            //looping through the array 
+            var result = 0.0;
+            foreach (double number in grades)
+            {
+                result += number;
+            }
+            //getting the average grade by dividing result and grades with the with count method
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result}");
+            Console.WriteLine(result);
+
+
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
