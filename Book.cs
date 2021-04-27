1c9a3b05fb6388049315a80936c9dc3695b96232
Faith Magras <bdanielle092@gmail.com>
2021-04-27 16:03:46 -0500

    going over loops and try/catch

--- Book.cs
+++ Book.cs
@@ -6,6 +6,7 @@ namespace GradeBook
     public class Book
     {
 
+
         //constructor - has the same name as the class and no return type
         public Book(string name)
         {
@@ -13,9 +14,35 @@ namespace GradeBook
 
             Name = name;
         }
+
+        public void AddLetterGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if (grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                throw new ArgumentException($"Invaid {nameof(grade)}");
+            }
         }
 
         public Statistics GetStatistics()
@@ -26,15 +53,35 @@ namespace GradeBook
             result.High = double.MinValue;
             result.Low = double.MaxValue;
 
-            foreach (double grade in grades)
-            {
-                result.Low = Math.Min(grade, result.Low);
-                result.High = Math.Max(grade, result.High);
 
-                result.Average += grade;
+
+            for (var index = 0; index < grades.Count; index += 1)
+            {
+                result.Low = Math.Min(grades[index], result.Low);
+                result.High = Math.Max(grades[index], result.High);
+                result.Average += grades[index];
+
             }
             //getting the average grade by dividing result and grades with the with count method
             result.Average /= grades.Count;
+            switch (result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
             return result;
         }
         private List<double> grades;
--- Program.cs
+++ Program.cs
@@ -12,13 +12,42 @@ namespace GradeBook
 
             //instantiaiting the book which mean creating a new object using the new keyword
             var book = new Book("Faith's Grade Book ");
-            book.AddGrade(89.1);
-            book.AddGrade(90.1);
-            book.AddGrade(77.1);
+
+
+            while (true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+                if (input == "q")
+                {
+                    break;
+                }
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
+
+
             var stats = book.GetStatistics();
             Console.WriteLine($"This is the lowest grade {stats.Low}");
             Console.WriteLine($"This is the highest grade {stats.High}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
 
 
         }
